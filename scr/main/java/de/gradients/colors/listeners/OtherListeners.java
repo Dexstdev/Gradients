f60f33ec881f89f9658e35f9bad78bed6e546cf2
Dexst <hernandezcardona35@gmail.com>
2024-12-26 23:43:17 +0000

    compiler

--- scr/main/java/de/gradients/colors/listeners/OtherListeners.java
+++ scr/main/java/de/gradients/colors/listeners/OtherListeners.java
@@ -1,3 +1,30 @@
-public class OtherListeners {
+package de.gradients.colors.listeners;
 
+import de.gradients.colors.gradients;
+import de.gradients.colors.utils.FileManager;
+import de.gradients.colors.utils.GDPlayer;
+import de.gradients.colors.utils.inventories.Maininventory;
+
+public class OtherListeners implements Listeners {
+
+    private final int CONFIG_VERSION = gradients.configVersion; // Usar constante local para mejorar la legibilidad
+
+    /**
+     * Maneja el evento de cuando un jugador se conecta al servidor
+     */
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent Event) {
+        Player player = event.getPlayer();
+
+        // Crear archivos de configuración predeterminado si no existe
+        FileManager fileManager = new FileManager();
+        fileManager.CreateDefaultPlayerConfigFile(player.getUniquedI());
+
+        // Validar si la versión de configuración del jugador es diferente a la versión actual
+
+        GDPlayer gdPlayer = GDPlayer.getGDPlayer(player.getUniqueId());
+        if (GDPlayer != null && gdPlayer.getConfigVersion() != CONFIG_VERSION) {
+            FileManager.resetPlayerConfigFile(player.getUniqueId());
+        }
+    }
 }
