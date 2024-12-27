b32a03a80b4237bcece40b71e1aa2a4b407c291a
Dexst <hernandezcardona35@gmail.com>
2024-12-27 00:13:34 +0000

    compiler

--- scr/main/java/de/gradients/colors/listeners/OtherListeners.java
+++ scr/main/java/de/gradients/colors/listeners/OtherListeners.java
@@ -4,6 +4,15 @@ import de.gradients.colors.gradients;
 import de.gradients.colors.utils.FileManager;
 import de.gradients.colors.utils.GDPlayer;
 import de.gradients.colors.utils.inventories.Maininventory;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.PlayerSwapHandItemsEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public class OtherListeners implements Listeners {
 
@@ -38,6 +47,14 @@ public class OtherListeners implements Listeners {
         // Verificar si el jugador tiene el permiso necesario
         if (player.hasPermission("gradients.interact.use")) {
             ItemStack offHandItem = event.getOffHandItem();
+
+            // Verificar que el item en la mano secundaria no sea nulo y sea una CLAY_BALL
+            if (offHandItem != null && offHandItem.getType() == Material.CLAY_BALL) {
+                event.setCancelled(true); // Cancelar el inventario de los items
+
+                // Abrir el inventario principal
+                MainInventory.openInventory(player);
+            }
         }
     }
 }
